be22683e258b9ae3be71a1e1e886cd561a1ac14a
yakkomajuri <yakko.majuri@gmail.com>
2021-10-27 14:11:09 +0000

    second iteration

--- index.ts
+++ index.ts
@@ -1,58 +1,81 @@
-import { Plugin } from '@posthog/plugin-scaffold'
+import { Plugin, PluginEvent } from '@posthog/plugin-scaffold'
 import fetch from 'node-fetch'
 
 export interface Migrator3000MetaInput {
+    global: {
+        startDate: string
+    },
     config: {
         host: string
-        project_api_key: string
+        projectApiKey: string
+        startDate: string
     }
 }
 
-const ONE_DAY = 1000 * 60 * 60 * 24
-
-interface MigrateEventsJobPayload {
-    'Start date': string
+interface PluginEventExtra extends PluginEvent {
+    $token?: string
+    project_id?: string
+    api_key?: string
 }
 
 const plugin: Plugin<Migrator3000MetaInput> = {
-    jobs: {
-        'Migrate events to a new instance': async (payload: MigrateEventsJobPayload, { jobs, storage }) => {
-            const cutoffDateExists = await storage.get('cutoff_date', null)
-            if (cutoffDateExists) {
-                console.log('Only one migration job can run at a time')
-                return
-            }
-            const cutoffDate = new Date(Date.now() + ONE_DAY * 2).toISOString()
-            await storage.set('cutoff_date', cutoffDate)
+
+    // every hour, check if we're done exporting the previous range and 
+    // if not, start a new job to export from the last max to now
+    runEveryHour: async ({ storage, jobs }) => {
+        const isExportRunning = await storage.get('is_export_running', false)
+        if (isExportRunning) {
+            return
+        }
+        const previousMaxDate = await storage.get('max_date', null)
+        if (!previousMaxDate) {
+            throw new Error('How did you get here?')
+        }
+
+        const newMaxDate = new Date(Date.now()).toISOString()
+        console.log(`Now starting export of events from ${previousMaxDate} to ${newMaxDate}`)
+        await storage.set('current_max_date', newMaxDate)
+        await jobs['Export historical events']({
+            dateFrom: previousMaxDate,
+            dateTo: newMaxDate,
+        }).runNow()
+    },
+
+    // used to provide near-immediate feedback to users that their export has started
+    runEveryMinute: async ({ global, jobs, utils, storage }) => {
+        await utils.cursor.init('initial_run')
+        const cursor = await utils.cursor.increment('initial_run')
+        if (cursor === 1) {
+            const maxDate = new Date(Date.now()).toISOString()
+            console.log(`Starting export of historical events from ${global.startDate}`)
+            await storage.set('current_max_date', maxDate)
             await jobs['Export historical events']({
-                dateFrom: payload['Start date'],
-                dateTo: cutoffDate,
+                dateFrom: global.startDate,
+                dateTo: maxDate,
             }).runNow()
-        },
+        }
     },
-    setupPlugin: async ({ storage }) => {
-        await storage.set('block_live_export', true)
+    
+    setupPlugin: async ({ config, global }) => {
+        try {
+            global.startDate = new Date(config.startDate).toISOString()
+        } catch (e) {
+            console.log(`Failed to parse start date. Make sure to use the format YYYY-MM-DD`)
+            throw e
+        }
     },
-    exportEvents: async (events, { config, storage }) => {
+    exportEvents: async (events: PluginEventExtra[], { config }) => {
+
+        // dont export live events, only historical ones
+        if (events.length > 0 && events[0].uuid) {
+            return
+        }
         const batch = []
         for (const event of events) {
-            const isLiveExportBlocked = await storage.get('block_live_export', false)
-            const timestamp = new Date(event.timestamp || Date.now())
-            const cutoffDate = await storage.get('cutoff_date', null)
-
-            if (cutoffDate && timestamp > new Date(String(cutoffDate))) {
-                await storage.set('block_live_export', false)
-                await storage.del('cutoff_date')
-                console.log('now coming to you liveeeeeee')
-            } else if (event.uuid && isLiveExportBlocked) {
-                return
-            }
-
-
             // eslint-disable-next-line @typescript-eslint/no-unused-vars
             const { team_id, now, offset, sent_at, $token, project_id, api_key, ...sendableEvent } = {
                 ...event,
-                token: config.project_api_key,
+                token: config.projectApiKey,
             }
             batch.push(sendableEvent)
         }
